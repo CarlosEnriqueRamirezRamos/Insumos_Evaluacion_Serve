a2825d564b1052bc8b058e3f93991fbaa11c36b9
alien 4 <alien 4@ALIEN4>
2025-07-01 18:28:08 -0600

    Primer Comit Server 01/07/2025

---
+++ src/main/java/com/Insumos/Evaluacion/Insumos_Evaluacion_Server/InsumosEvaluacionServerApplication.java
@@ -0,0 +1,13 @@
+package com.Insumos.Evaluacion.Insumos_Evaluacion_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsumosEvaluacionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsumosEvaluacionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Insumos/Evaluacion/Insumos_Evaluacion_Server/InsumosEvaluacionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insumos.Evaluacion.Insumos_Evaluacion_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsumosEvaluacionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
